539ee24a986df599431e2e1339795f2fbf664d3e
Abdul Mateen <abdulmateenkhanzwl@gmail.com>
2022-03-29 10:40:18 +0500

    clock

--- tasks/Program.cs
+++ tasks/Program.cs
@@ -11,17 +11,32 @@ namespace tasks
     {
         static void Main(string[] args)
         {
-            List<test> numbers = new List<test>();
-            for (int i = 0; i < 5; i++)
-            {
-                test r=new test();
-                r.num = i+1;
-                numbers.Add(r);
-            }
-            foreach(var i in numbers)
-            {
-                Console.WriteLine(i.num);
-            }
+            test test = new test();
+            Console.Write("Empty Time :> ");
+            test.print_time();
+            test test1 = new test(8);
+            Console.Write("Empty Time :> ");
+            test1.print_time();
+            test test2 = new test(8,30);
+            Console.Write("Empty Time :> ");
+            test2.print_time();
+            test test3 = new test(8,30,55);
+            Console.Write("Empty Time :> ");
+            test3.print_time();
+            test3.increment_hours();
+            Console.Write("Time :> ");
+            test3.print_time();
+            test3.increment_min();
+            Console.Write("Time :> ");
+            test3.print_time();
+            test3.increment_sec();
+            Console.Write("Time :> ");
+            test3.print_time();
+            bool flag=test3.is_equal(3,40, 55);
+            Console.WriteLine("Flag :> "+flag);
+            test clk = new test(9,31,56);
+            flag = test3.is_equal(clk);
+            Console.WriteLine("Flag :> " + flag);
             Console.ReadKey();
         }
     }
--- tasks/jk/test.cs
+++ tasks/jk/test.cs
@@ -8,6 +8,67 @@ namespace tasks.jk
 {
     internal class test
     {
-        public int num;
+        public int hr;
+        public int min;
+        public int sec;
+        public test()
+        {
+            hr = 0;
+            min = 0;
+            sec = 0;
+        }
+        public test(int h)
+        {
+            hr = h;
+        }
+        public test(int h,int m)
+        {
+            hr = h;
+            min = m;
+        }
+        public test(int h,int m,int s)
+        {
+            hr = h;
+            min = m;
+            sec = s;
+        }
+        public void increment_sec()
+        {
+            sec++;
+        }
+        public void increment_min()
+        {
+            min++;
+        }
+        public void increment_hours()
+        {
+            hr++;
+        }
+        public void print_time()
+        {
+            Console.WriteLine(hr+" : "+min+" : "+sec);
+        }
+        public bool is_equal(int h,int m,int s)
+        {
+            if(hr==h &&min==m&&sec==s)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool is_equal(test ch)
+        {
+            if (hr == ch.hr && min == ch.min && sec == ch.sec)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
