f69bd6f942d20689f692b1ab1ba8c466deaa199c
Abdul Mateen <abdulmateenkhanzwl@gmail.com>
2022-03-29 10:02:16 +0500

    Add project files.

---
+++ tasks/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tasks.jk;
+
+namespace tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> num = new List<int>() { 1,2,3,4,5,6,7,8,9};
+            for (int i = 0; i < num.Count; i++)
+            {
+                Console.WriteLine(num[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ tasks/jk/test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tasks.jk
+{
+    internal class test
+    {
+        public test()
+        {
+            name = "MATEEN";
+        }
+        public test(test to_copy)
+        {
+            name=to_copy.name;
+            age= to_copy.age;
+            marks= to_copy.marks;
+            gpa= to_copy.gpa;
+        }
+        public string name;
+        public int age;
+        public int marks;
+        public float gpa;
+    }
+}
